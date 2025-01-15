a8887dcb9c049b2779e1fb9d78ae2a6840786d11
SUDHAPANTHA <sudhapantha111@gmail.com>
2025-01-15 22:12:18 +0545

    edit and delete

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post-detail'),
+    path('post/<int:pk>/', views.post_edit, name='post-edit'),
+    path('post/<int:pk>/', views.post_delete, name='post-delete'),
 ]
 
 if settings.DEBUG:
--- blog_app/views.py
+++ blog_app/views.py
@@ -9,3 +9,9 @@ def post_list(request):
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
     return render(request, "post-detail.html", {"post": post})
+def post_edit(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, "post-edit.html", {"post": post})
+def post_delete(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, "post-delete.html", {"post": post})
