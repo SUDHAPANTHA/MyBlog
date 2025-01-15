33c1a5b494ca7ce07caac3b2aa617ceb58b72d21
SUDHAPANTHA <sudhapantha111@gmail.com>
2025-01-15 22:48:07 +0545

    completed edit and delete

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post-detail'),
-    path('post/<int:pk>/', views.post_edit, name='post-edit'),
-    path('post/<int:pk>/', views.post_delete, name='post-delete'),
+    path("post_edit/<int:id>/", views.post_edit, name="post-edit"),
+    path('post_delete/<int:pk>/', views.post_delete, name='post-delete'),
 ]
 
 if settings.DEBUG:
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from blog_app.models import Post
@@ -9,9 +10,15 @@ def post_list(request):
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
     return render(request, "post-detail.html", {"post": post})
-def post_edit(request, pk):
-    post = Post.objects.get(pk=pk)
-    return render(request, "post-edit.html", {"post": post})
+def post_edit(request, id):
+            post = Post.objects.get(id=id)
+            if request.method == "GET":
+                return render(request, "post-edit.html", {"post": post},)
+            else:
+                post.title = request.POST["title"]
+                post.save()
+                return HttpResponseRedirect("/")
 def post_delete(request, pk):
-    post = Post.objects.get(pk=pk)
-    return render(request, "post-delete.html", {"post": post})
+    todo = Post.objects.get(id=pk)
+    todo.delete()
+    return HttpResponseRedirect("/")
