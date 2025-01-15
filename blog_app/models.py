a42403da9cb3f99b7923c6fd89ba07bac74646a6
SUDHAPANTHA <sudhapantha111@gmail.com>
2025-01-15 11:02:32 +0545

    first commit

---
+++ blog_app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+CHOICES =[
+    ("active","Active"),
+    ("inactive","Inactive")
+]
+
+class Post(models.Model):
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    image = models.ImageField(upload_to="images/", blank=False, null=False)
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=CHOICES, default="active")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(null=True, blank=True)
+
+# Create your models here.
+def __str__(self):
+    return self.title
---
+++ blog_app/urls.py
@@ -0,0 +1,7 @@
+from blog_app import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post-detail'),
+]
