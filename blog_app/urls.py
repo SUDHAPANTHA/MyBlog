02a79072b401f4b69003d005d1d2dba70c6668bb
SUDHAPANTHA <sudhapantha111@gmail.com>
2025-01-16 09:33:07 +0545

    changes on url

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post-detail'),
+    path('post-detail/<int:pk>/', views.post_detail, name='post-detail'),
     path("post_edit/<int:id>/", views.post_edit, name="post-edit"),
     path('post_delete/<int:pk>/', views.post_delete, name='post-delete'),
 ]
