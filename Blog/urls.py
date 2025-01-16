2b3a951e36ce28c3c1074345902b38f5250ae66a
SUDHAPANTHA <sudhapantha111@gmail.com>
2025-01-16 09:55:11 +0545

    login and logout

--- Blog/urls.py
+++ Blog/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views #login and logout ko lagi
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog_app.urls')),
+    path("accounts/login/", views.LoginView.as_view(), name="login"),
+    path("accounts/logout/", views.LogoutView.as_view(), name="logout"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
