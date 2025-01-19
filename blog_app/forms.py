34d4df6c446f616f75c4ecb38dec7401ddc3b76c
SUDHAPANTHA <sudhapantha111@gmail.com>
2025-01-19 12:11:01 +0545

    ui

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -1,4 +1,3 @@
-
 from django import forms
 from blog_app.models import Post
 
@@ -6,3 +5,30 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content", "image"]
+
+        widgets = {
+            "title": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Enter the title of the post",
+                }
+            ),
+            "content": forms.Textarea(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Write the content here...",
+                    "rows": 6,
+                }
+            ),
+            "image": forms.ClearableFileInput(
+                attrs={
+                    "class": "form-control",
+                }
+            ),
+        }
+
+        labels = {
+            "title": "Post Title",
+            "content": "Post Content",
+            "image": "Upload Image",
+        }
